f08582f21b0edad19bca7de92838c52e0de58d4b
Navibytes <nappingnavi@gmail.com>
2025-03-24 22:17:22 -0500

    first commit

---
+++ src/App.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class App {
+    static boolean killer = false;
+    public static void main(String[] args) throws Exception {
+        System.out.println("Welcome to To-do program!");
+        Scanner reader = new Scanner(System.in);
+        while (killer){
+            String program = reader.nextLine();
+             
+        }
+
+        reader.close();
+    }
+}
---
+++ src/file.java
@@ -0,0 +1,8 @@
+import java.time.*;
+public class file {
+    int id;
+    String description;
+    String status;
+    String createdAt;
+    String updatedAt;
+}
