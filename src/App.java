1e56cb96c391a0ffaab2dc1d521566c2a7bfcf1f
Diego Lozano <nappingnavi@gmail.com>
2025-04-11 13:27:26 -0500

    working edit function

--- src/App.java
+++ src/App.java
@@ -1,12 +1,15 @@
 import java.util.*;
+import java.lang.reflect.Array;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 public class App {
     static boolean killer = true;
     public static void main(String[] args) throws Exception {
         System.out.println("Welcome to To-do program!");
         Scanner reader = new Scanner(System.in);
-        int ID = 1;
+        int ID = 0;
+        ArrayList<task> taskList =  new ArrayList<task>();
         while (killer){
             System.out.println("Kill program with 'kill'");
             String program = reader.nextLine();
@@ -18,17 +21,21 @@ public class App {
                 System.out.println(program);
                 program = program.substring(program.indexOf("d")+2, program.length());
                 System.out.println(program);
-                task task = new task(ID, program);
-                System.out.println("Task added succesfully (ID: "+ID+")"+program);
+                LocalDateTime clock = LocalDateTime.now();
+                task task = new task(ID, program, clock);
+                taskList.add(task);
+                System.out.println("Task added succesfully (ID: "+ID+")"+program+clock);
                 ++ID;
             }
-            if (program.startsWith("edit task ID")){
+            if (program.startsWith("update ")){
                 program = program.replaceAll("\\s", "");
                 System.out.println(program);
-                program = program.substring(program.indexOf("D")+1, program.length());
+                program = program.substring(program.indexOf("e")+1, program.length());
                 System.out.println(program);
                 int inputID = Integer.parseInt(program);
                 System.out.println("you are editing task"+ inputID);
+                LocalDateTime clock = LocalDateTime.now();
+                taskList.get(inputID).update(clock);
             }
         }
 
--- src/task.java
+++ src/task.java
@@ -1,13 +1,23 @@
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 public class task {
     int id;
     String description="";
     String status = "haven't started";
+    LocalDateTime createdAt;
+    LocalDateTime updatedAt;
 
-    task(int ID, String desc ){
+
+    task(int ID, String desc, LocalDateTime createdAt){
         id = ID;
         description = desc;
+        this.createdAt = createdAt;
+    }
+
+    public void update(LocalDateTime updatedAtVar){
+        updatedAt = updatedAtVar;
+        System.out.println("New time is: "+ updatedAt);
     }
 
     public void mark(boolean done){
